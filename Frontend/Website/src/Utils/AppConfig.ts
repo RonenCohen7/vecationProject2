4db970d6a26df6a8dfc05c3a6ed971b020ca1eb0
Ronen Cohen <Ronenc7@gmail.com>
2026-04-14 19:01:00 +0300

    fix before rebase

--- Frontend/Website/src/Utils/AppConfig.ts
+++ Frontend/Website/src/Utils/AppConfig.ts
@@ -1,22 +1,14 @@
-    class AppConfig {
+class AppConfig {
+    private readonly host = import.meta.env.VITE_API_URL || "http://localhost:4003";
 
-        // public readonly isDevelopment = (import.meta.env.MODE === "development");
-        public readonly isDevelopment = import.meta.env.DEV;
-        private readonly devHost = "http://localhost:4003";
-        private readonly prodHost = "http://35.176.62.102:4003";
+    public readonly vacationUrl = this.host + "/api/vacation/vacation";
+    public readonly loginUrl = this.host + "/api/auth/login";
+    public readonly registerUrl = this.host + "/api/auth/register";
+    public readonly likesUrl = this.host + "/api/likes/likes";
+    public readonly likesCount = this.host + "/api/likes/likes/count";
+    public readonly feedUrl = this.host + "/api/feed";
+    public readonly imageUrl = this.host + "/images/";
+    public readonly bookingUrl = this.host + "/api/booking";
+}
 
-        private readonly host = this.isDevelopment ? this.devHost : this.prodHost;
-
-        public readonly vacationUrl = this.host + "/api/vacation/vacation";
-        public readonly loginUrl = this.host + "/api/auth/login";
-        public readonly registerUrl = this.host + "/api/auth/register";
-        public readonly likesUrl = this.host + "/api/likes/likes";
-        public readonly likesCount = this.host + "/api/likes/likes/count";
-        public readonly feedUrl = this.host + "/api/feed";
-        public readonly imageUrl = this.host + "/images/";
-        public readonly bookingUrl = this.host + "/api/booking"
-
-      
-    }
-
-    export const appConfig = new AppConfig();
+export const appConfig = new AppConfig();
